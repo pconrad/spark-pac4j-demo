4ec43b0f956db1806a045606875a2a5dd75e6724
Phill Conrad <pconrad@cs.ucsb.edu>
2016-06-08 14:16:06 -0700

    more simplification

--- src/main/java/org/pac4j/demo/spark/DemoConfigFactory.java
+++ src/main/java/org/pac4j/demo/spark/DemoConfigFactory.java
@@ -1,32 +1,19 @@
 package org.pac4j.demo.spark;
 
-// import org.pac4j.cas.client.CasClient;
 import org.pac4j.core.authorization.RequireAnyRoleAuthorizer;
 import org.pac4j.core.client.Clients;
 import org.pac4j.core.config.Config;
 import org.pac4j.core.config.ConfigFactory;
 import org.pac4j.core.matching.ExcludedPathMatcher;
-import org.pac4j.http.client.direct.DirectBasicAuthClient;
-import org.pac4j.http.client.direct.ParameterClient;
-// import org.pac4j.http.client.indirect.FormClient;
-// import org.pac4j.http.client.indirect.IndirectBasicAuthClient;
-// import org.pac4j.http.credentials.authenticator.test.SimpleTestUsernamePasswordAuthenticator;
-// import org.pac4j.jwt.credentials.authenticator.JwtAuthenticator;
 import org.pac4j.oauth.client.FacebookClient;
-// import org.pac4j.oauth.client.TwitterClient;
-// import org.pac4j.oidc.client.OidcClient;
-// import org.pac4j.saml.client.SAML2Client;
-// import org.pac4j.saml.client.SAML2ClientConfiguration;
+import org.pac4j.oauth.client.TwitterClient;
 import spark.TemplateEngine;
 
 public class DemoConfigFactory implements ConfigFactory {
 
-    private final String salt;
-
     private final TemplateEngine templateEngine;
 
-    public DemoConfigFactory(final String salt, final TemplateEngine templateEngine) {
-        this.salt = salt;
+    public DemoConfigFactory(final TemplateEngine templateEngine) {
         this.templateEngine = templateEngine;
     }
 
@@ -46,7 +33,7 @@ public class DemoConfigFactory implements ConfigFactory {
 
         final Config config = new Config(clients); // keep this
 
-        config.addAuthorizer("admin", new RequireAnyRoleAuthorizer("ROLE_ADMIN"));
+        // config.addAuthorizer("admin", new RequireAnyRoleAuthorizer("ROLE_ADMIN"));
         config.addMatcher("excludedPath", new ExcludedPathMatcher("^/facebook/notprotected$")); // keep this
         config.setHttpActionAdapter(new DemoHttpActionAdapter(templateEngine)); // keep this
         return config;
--- src/main/java/org/pac4j/demo/spark/SparkPac4jDemo.java
+++ src/main/java/org/pac4j/demo/spark/SparkPac4jDemo.java
@@ -7,10 +7,8 @@ import org.pac4j.core.client.Clients;
 import org.pac4j.core.config.Config;
 import org.pac4j.core.profile.ProfileManager;
 import org.pac4j.core.profile.UserProfile;
-import org.pac4j.http.client.indirect.FormClient;
 
-import org.pac4j.jwt.profile.JwtGenerator;
-import org.pac4j.saml.client.SAML2Client;
+
 import org.pac4j.sparkjava.ApplicationLogoutRoute;
 import org.pac4j.sparkjava.CallbackRoute;
 import org.pac4j.sparkjava.RequiresAuthenticationFilter;
@@ -28,7 +26,6 @@ import static spark.Spark.*;
 @SuppressWarnings({"unchecked"})
 public class SparkPac4jDemo {
 
-	private final static String JWT_SALT = "12345678901234567890123456789012";
 
 	private final static Logger logger = LoggerFactory.getLogger(SparkPac4jDemo.class);
 
@@ -36,51 +33,18 @@ public class SparkPac4jDemo {
 
 	public static void main(String[] args) {
 		port(8080);
-		final Config config = new DemoConfigFactory(JWT_SALT, templateEngine).build();
+		final Config config = new DemoConfigFactory(templateEngine).build();
 
 		get("/", SparkPac4jDemo::index, templateEngine);
 		final Route callback = new CallbackRoute(config);
 		get("/callback", callback);
 		post("/callback", callback);
-        final RequiresAuthenticationFilter facebookFilter = new RequiresAuthenticationFilter(config, "FacebookClient", "", "excludedPath");
+        final RequiresAuthenticationFilter 
+			facebookFilter = new RequiresAuthenticationFilter(config, "FacebookClient", "", "excludedPath");
         before("/facebook", facebookFilter);
 		before("/facebook/*", facebookFilter);
-		before("/facebookadmin", new RequiresAuthenticationFilter(config, "FacebookClient", "admin"));
-		before("/facebookcustom", new RequiresAuthenticationFilter(config, "FacebookClient", "custom"));
-
-		/*		before("/twitter", new RequiresAuthenticationFilter(config, "TwitterClient,FacebookClient"));
-		before("/form", new RequiresAuthenticationFilter(config, "FormClient"));
-		before("/basicauth", new RequiresAuthenticationFilter(config, "IndirectBasicAuthClient"));
-		before("/cas", new RequiresAuthenticationFilter(config, "CasClient"));
-		before("/saml2", new RequiresAuthenticationFilter(config, "SAML2Client"));
-		before("/oidc", new RequiresAuthenticationFilter(config, "OidcClient"));
-		before("/protected", new RequiresAuthenticationFilter(config, null));
-		before("/dba", new RequiresAuthenticationFilter(config, "DirectBasicAuthClient,ParameterClient"));
-		before("/rest-jwt", new RequiresAuthenticationFilter(config, "ParameterClient")); */
 
 		get("/facebook", SparkPac4jDemo::protectedIndex, templateEngine);
-        get("/facebook/notprotected", SparkPac4jDemo::protectedIndex, templateEngine);
-		get("/facebookadmin", SparkPac4jDemo::protectedIndex, templateEngine);
-		get("/facebookcustom", SparkPac4jDemo::protectedIndex, templateEngine);
-
-		/*		get("/twitter", SparkPac4jDemo::protectedIndex, templateEngine);
-		get("/form", SparkPac4jDemo::protectedIndex, templateEngine);
-		get("/basicauth", SparkPac4jDemo::protectedIndex, templateEngine);
-		get("/cas", SparkPac4jDemo::protectedIndex, templateEngine);
-		get("/saml2", SparkPac4jDemo::protectedIndex, templateEngine);
-		get("/saml2-metadata", (rq, rs) -> {
-			SAML2Client samlclient = config.getClients().findClient(SAML2Client.class);
-			samlclient.init(new SparkWebContext(rq, rs));
-			return samlclient.getServiceProviderMetadataResolver().getMetadata();
-		});
-		get("/jwt", SparkPac4jDemo::jwt, templateEngine);
-		get("/oidc", SparkPac4jDemo::protectedIndex, templateEngine);
-		get("/protected", SparkPac4jDemo::protectedIndex, templateEngine);
-		get("/dba", SparkPac4jDemo::protectedIndex, templateEngine);
-		get("/rest-jwt", SparkPac4jDemo::protectedIndex, templateEngine);
-		get("/loginForm", (rq, rs) -> form(config.getClients()), templateEngine); */
-
-		get("/logout", new ApplicationLogoutRoute(config));
 
 		exception(Exception.class, (e, request, response) -> {
 			logger.error("Unexpected exception", e);
@@ -93,28 +57,6 @@ public class SparkPac4jDemo {
 		map.put("profile", getUserProfile(request, response));
 		return new ModelAndView(map, "index.mustache");
 	}
-
-	/*
-	private static ModelAndView jwt(final Request request, final Response response) {
-		final UserProfile profile = getUserProfile(request, response);
-		JwtGenerator generator = new JwtGenerator(JWT_SALT);
-		String token = "";
-		if (profile != null) {
-			token = generator.generate(profile);
-		}
-		final Map map = new HashMap();
-		map.put("token", token);
-		return new ModelAndView(map, "jwt.mustache");
-		} */
-
-	/*
-	private static ModelAndView form(final Clients clients) {
-		final Map map = new HashMap();
-		final FormClient formClient = clients.findClient(FormClient.class);
-		map.put("callbackUrl", formClient.getCallbackUrl());
-		return new ModelAndView(map, "loginForm.mustache");
-	}
-	*/
 
 	private static ModelAndView protectedIndex(final Request request, final Response response) {
 		final Map map = new HashMap();
